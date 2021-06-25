ec44a596a385dd067610533552cc788a4e290f4e
Mike Lloyd <mike.lloyd@teradata.com>
2021-06-25 09:32:31 -0700

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #! /bin/sh
 
-<<<<<<< HEAD
-dotfiles=(.vim .vimrc .tmux .tmux.conf .config/coc .config/xkbcomp .zshrc .oh-my-zsh .local/share/zsh .config/karabiner)
-=======
-set -x
+# set -x
 
-dotfiles=(.vim .vimrc .tmux .tmux.conf .config/coc .config/fish .config/omf)
+dotfiles=(.vim .vimrc .tmux .tmux.conf .config/coc .zshrc .oh-my-zsh .local/share/zsh)
 linux_dotfiles=(.config/xkbcomp)
 mac_dotfiles=(.config/karabiner)
 
@@ -17,20 +14,6 @@ else
     echo "Cannot deploy on this system."
     exit 1
 fi
-
-# Check if fish is installed.
-which fish > /dev/null 2>&1
-if [ $? = 0 ]; then
-    # Check if oh-my-fish is installed. If not, install it.
-    which omf > /dev/null 2>&1
-    if [ $? != 0 ]; then
-        curl -L https://get.oh-my.fish | fish
-    fi
-else
-    echo "fish must be installed to use this script."
-    exit 1
-fi
->>>>>>> f2914d101349ad97208caed27b58ab2988cae0cf
 
 echo "Deploying .dotfiles..."
 for f in "${dotfiles[@]}"
