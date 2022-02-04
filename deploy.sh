d141bbd7fa891a2dd09d8236a521c0d9698d09f4
Mike (work dev) <mike.lloyd03@pm.me>
2022-02-04 09:35:05 -0800

    Remove .xinitrc from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,31 +16,31 @@ prefix_err="${RD}[.dotfiles deploy]${NM}"
 script_location="$(cd "$(dirname "$0")" && pwd -P)"
 
 dotfiles=(.vim .vimrc .tmux .tmux.conf .config/coc .zshrc .oh-my-zsh .local/share/zsh .config/starship.toml .gitignore-global)
-linux_dotfiles=(.xinitrc .config/xkbcomp)
+linux_dotfiles=(.config/xkbcomp)
 mac_dotfiles=(.config/karabiner)
 
 if [ "$(uname -s)" = Linux ]; then
-    dotfiles+=("${linux_dotfiles[@]}")
+  dotfiles+=("${linux_dotfiles[@]}")
 elif [ "$(uname -s)" = Darwin ]; then
-    dotfiles+=("${mac_dotfiles[@]}")
+  dotfiles+=("${mac_dotfiles[@]}")
 else
-    echo "$prefix_err Cannot deploy on this system."
-    exit 1
+  echo "$prefix_err Cannot deploy on this system."
+  exit 1
 fi
 
 echo "$prefix Deploying .dotfiles..."
 for f in "${dotfiles[@]}"; do
-    if [ -e "$HOME/$f" ]; then
-        if [ -L "$HOME/$f" ] && [ "$(readlink "$HOME/$f")" = "$script_location/$f" ]; then
-            echo "$prefix_warn Link for $f already exists. Skipping."
-            continue
-        fi
-        echo "$prefix_warn $f already exists. Backing up."
-        mv "$HOME/$f" "$HOME/$f.pre-deploy"
+  if [ -e "$HOME/$f" ]; then
+    if [ -L "$HOME/$f" ] && [ "$(readlink "$HOME/$f")" = "$script_location/$f" ]; then
+      echo "$prefix_warn Link for $f already exists. Skipping."
+      continue
     fi
-    echo "$prefix Creating link for $f"
-    mkdir -p "$(dirname "$HOME/$f")"
-    ln -s "$script_location/$f" "$HOME/$f"
+    echo "$prefix_warn $f already exists. Backing up."
+    mv "$HOME/$f" "$HOME/$f.pre-deploy"
+  fi
+  echo "$prefix Creating link for $f"
+  mkdir -p "$(dirname "$HOME/$f")"
+  ln -s "$script_location/$f" "$HOME/$f"
 done
 
 # Install vim-plugs
@@ -48,10 +48,10 @@ done
 
 # Setup global .gitignore file
 if [ -z "$(git config --global core.excludesfile)" ]; then
-    echo "$prefix Setting global .gitignore variable"
-    git config --global core.excludesfile "$HOME/.gitignore-global"
+  echo "$prefix Setting global .gitignore variable"
+  git config --global core.excludesfile "$HOME/.gitignore-global"
 else
-    echo "$prefix_warn Global .gitignore variable already set."
+  echo "$prefix_warn Global .gitignore variable already set."
 fi
 
 echo "$prefix Done."
