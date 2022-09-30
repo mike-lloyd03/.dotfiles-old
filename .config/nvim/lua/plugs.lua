f32506cc00862b8cf03e78cdfa53704211bc5239
Mike Lloyd <mike.lloyd03@pm.me>
2022-09-30 12:16:32 -0700

    Scroll less

--- .config/nvim/lua/plugs.lua
+++ .config/nvim/lua/plugs.lua
@@ -328,6 +328,6 @@ require("neoscroll").setup({
 })
 
 local t = {}
-t["<C-k>"] = { "scroll", { "-10", "true", "250" } }
-t["<C-j>"] = { "scroll", { "10", "true", "250" } }
+t["<C-k>"] = { "scroll", { "-5", "true", "250" } }
+t["<C-j>"] = { "scroll", { "5", "true", "250" } }
 require("neoscroll.config").set_mappings(t)
