f544faaa9810c55e6816d0ffd3d3bcd72d71bc1c
Mike Lloyd <mike.lloyd03@pm.me>
2023-10-29 22:58:53 -0700

    Update wezterm font

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,8 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
+config.font_size = 10
+
 config.hide_tab_bar_if_only_one_tab = true
 
 config.color_scheme = "One Dark (Gogh)"
@@ -13,6 +15,13 @@ config.color_scheme = "One Dark (Gogh)"
 config.colors = {
     foreground = "#a0a8b7",
     background = "#1a1c23",
+}
+
+config.window_padding = {
+    left = "0cell",
+    right = "0cell",
+    top = "0cell",
+    bottom = "0cell",
 }
 
 return config
