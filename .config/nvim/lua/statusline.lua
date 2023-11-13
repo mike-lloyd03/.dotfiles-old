40a184836f27b7614a36fd7ee8a3836b6da5c740
Mike Lloyd <mike.lloyd03@pm.me>
2023-11-12 20:19:43 -0800

    Update zellij statusline and switch to slanted style

--- .config/nvim/lua/statusline.lua
+++ .config/nvim/lua/statusline.lua
@@ -12,8 +12,10 @@ require("lualine").setup({
     options = {
         icons_enabled = true,
         theme = "onedark",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
+        -- component_separators = { left = "", right = "" },
+        -- section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
         disabled_filetypes = { "NvimTree" },
         always_divide_middle = true,
         -- globalstatus = true,
